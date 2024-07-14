bdb327359510d222fac6e2843435490f4fe95d70
jagedos <jagedosusrecomm@gmail.com>
2024-07-14 12:20:19 +0300

    code grade done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun("bathe my dog")
+
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+ 
+function wrapAdjective (special ="*") {
+    return function (adjective = "a hard worker"){
+        return `You are ${special}${adjective}${special}!`;
+    };
+}
+
+wrapAdjective("%")("a dedicated programmer");
